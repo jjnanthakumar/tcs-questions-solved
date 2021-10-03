3e87221bda7d75c2268c257c240e0007271b6969
Nanthakumar J J <46053893+jjnanthakumar@users.noreply.github.com>
2021-10-03 13:35:06 +0530

    Added two new solutions

---
+++ TCS DCA/stock.py
@@ -0,0 +1,19 @@
+store = {
+    101: ["Milk", 42, 10],
+    102: ["Cheese", 50, 20],
+    103: ["Ghee", 500,40],
+    104: ["Bread", 40, 16]
+}
+
+item = int(input())
+quantity = int(input())
+if (store.get(item) is None):
+    print("INVALID INPUT")
+else:
+    if (quantity<store[item][-1]):
+        print("{:.1f} INR".format(store[item][1]*quantity))
+        store[item][-1] -= quantity
+        print(f"{store[item][-1]} LEFT")
+    elif store[item][-1]<quantity:
+        print("NO STOCK")
+        print(f"{store[item][-1]} LEFT")
---
+++ TCS DCA/totalParticipants.py
@@ -0,0 +1,7 @@
+n = int(input())
+if n&1:
+    res = [str(n//4) if i!=3 else str((n//4)+n%4) for i in range(4)]
+else:
+    res = [str(n//4) for _ in range(4)]
+
+print("\n".join(res))
